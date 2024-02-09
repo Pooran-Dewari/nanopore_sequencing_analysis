2964069757a0f8fdbd24d60a24f65fc345cdaa5e
Pooran Dewari <67807603+Pooran-Dewari@users.noreply.github.com>
2024-02-09 17:26:41 +0000

    check if empty fastq file before merging

--- 02_merge_fastq/merge_fastq.sh
+++ 02_merge_fastq/merge_fastq.sh
@@ -1,6 +1,7 @@
 for barcode in */
 
 do
+
  echo "merging $barcode"
  # step1: merge fastq.gz files
  subdir=$(basename "$barcode")
@@ -15,6 +16,12 @@ do
  last=$(awk '$2 ~ /total/ { print $1 }' wc.txt)
  echo $first
  echo $last
+
+ # Check if either merged file or input files are empty!
+ if (( first == 0 || last == 0 )); then
+     echo "Warning: either merged or input fastq files are empty!!"
+ fi
+
  # Check if merged file line count is equal to sum of the rest files
  if (( last == 2*first )); then
     echo "files merged successfully"
@@ -25,4 +32,5 @@ do
  cd ..
  echo "............."
  echo "            "
+
 done
