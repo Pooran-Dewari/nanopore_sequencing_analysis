0bcee29da50124bc06c83c8ec51bd504b8284f96
Pooran Dewari <67807603+Pooran-Dewari@users.noreply.github.com>
2024-02-09 18:03:26 +0000

    Update merge_fastq.sh

--- 02_merge_fastq/merge_fastq.sh
+++ 02_merge_fastq/merge_fastq.sh
@@ -14,12 +14,10 @@ do
  # Use awk to extract the first and last integers from the wc.txt file
  first=$(awk  -v pat="$subdir" '$2 ~ pat { print $1 }' wc.txt)
  last=$(awk '$2 ~ /total/ { print $1 }' wc.txt)
- echo $first
- echo $last
 
  # Check if either merged file or input files are empty!
  if (( first == 0 || last == 0 )); then
-     echo "Warning: either merged or input fastq files are empty!!"
+     echo "Warning: check if fastq files are empty!!"
  fi
 
  # Check if merged file line count is equal to sum of the rest files
@@ -28,7 +26,7 @@ do
  else
     echo "file merge error"
  fi
- #rm wc.txt
+
  cd ..
  echo "............."
  echo "            "
