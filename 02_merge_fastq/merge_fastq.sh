259dfad66f74dac1dd5d434048aa50fc33237929
Pooran Dewari <67807603+Pooran-Dewari@users.noreply.github.com>
2024-02-09 17:21:59 +0000

    Create merge_fastq.sh

---
+++ 02_merge_fastq/merge_fastq.sh
@@ -0,0 +1,28 @@
+for barcode in */
+
+do
+ echo "merging $barcode"
+ # step1: merge fastq.gz files
+ subdir=$(basename "$barcode")
+ cat $subdir/*.fastq.gz > $subdir/"$subdir".fastq.gz
+
+ # step2: validate merged file
+ cd $subdir
+ wc -l *gz > wc.txt
+
+ # Use awk to extract the first and last integers from the wc.txt file
+ first=$(awk  -v pat="$subdir" '$2 ~ pat { print $1 }' wc.txt)
+ last=$(awk '$2 ~ /total/ { print $1 }' wc.txt)
+ echo $first
+ echo $last
+ # Check if merged file line count is equal to sum of the rest files
+ if (( last == 2*first )); then
+    echo "files merged successfully"
+ else
+    echo "file merge error"
+ fi
+ #rm wc.txt
+ cd ..
+ echo "............."
+ echo "            "
+done
