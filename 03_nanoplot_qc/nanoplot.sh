2097bbab6e7e9f26076fb2d26a3117909e160040
Pooran Dewari <67807603+Pooran-Dewari@users.noreply.github.com>
2024-02-21 10:09:31 +0000

    Create nanoplot.sh

---
+++ 03_nanoplot_qc/nanoplot.sh
@@ -0,0 +1,9 @@
+ls *gz  > fastq_files.txt
+
+OUTPUT="nanoplot_qc"
+mkdir $OUTPUT
+
+while read FASTQ;
+do
+ NanoPlot -t2 --fastq $FASTQ --loglength -o $OUTPUT/"$FASTQ"_NanoPlot_Summary
+done < fastq_files.txt
